9cfd69546992afa80440a94d96b286a9a94dfced
tyrdom <tyrdom@hotmail.com>
2020-04-06 20:12:06 +0800

    StartProj

---
+++ hang_up/GameServers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Akka.Actor;
+namespace GameServers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("GameServers");
+            
+        }
+    }
+}
